d1d60665634c3fc89f3fd102843cfd7fa5bc842a
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 10:58:02 +0300

    Initial commit

---
+++ FoodorDrink/Program.cs
@@ -0,0 +1,28 @@
+﻿class FoodorDrink
+{
+    public static void Main(string[] args)
+    {
+        string product = Console.ReadLine();
+
+        if (product == "curry" || product == "noodles"
+                               || product == "sushi"
+                               || product == "spaghetti"
+                               || product == "bread")
+        {
+
+            Console.WriteLine("food");
+        }
+        else if (product == "tea" || product == "water"
+                                  || product == "coffee"
+                                  || product=="juice")
+        { 
+            Console.WriteLine("drink");
+        }
+        else
+        { 
+                        Console.WriteLine("unknown");
+        }
+    }
+}
+
+                    
